5d084f6935af88db728fdbff70673faf8a4aab3d
unknown <Sergio.Krapchini@yandex.ru>
2023-03-06 15:33:06 +0300

    доработка стилей PART 3

--- src/components/Favorite/FavoriteTitle/FavoriteTitle.jsx
+++ src/components/Favorite/FavoriteTitle/FavoriteTitle.jsx
@@ -1,14 +1,16 @@
+/* eslint-disable react/void-dom-elements-no-children */
 /* eslint-disable no-underscore-dangle */
 import { useQuery } from '@tanstack/react-query'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { dogShopApi } from '../../../api/DogShopApi'
 import { getFavoriteSelector } from '../../../redux/slices/favoriteSlice'
 import { getTokenSelector } from '../../../redux/slices/userSlice'
 import { Loader } from '../../Loader/Loader'
 import { FavoriteCard } from '../FavoriteCard/FavoriteCard'
 import favoriteTitleStyle from './FavoriteTitle.module.css'
+import logo from '../../Img/logoTwo.png'
 
 export function FavoriteTitle() {
   const token = useSelector(getTokenSelector)
@@ -62,30 +64,53 @@ export function FavoriteTitle() {
   }
 
   return (
-    <div className={favoriteTitleStyle.favoriteTitleBlockAll}>
-      <div className={favoriteTitleStyle.favoriteTitleHeaderText}>
-        <h2>● Избранное ●</h2>
-      </div>
-      <div className={favoriteTitleStyle.favoriteTitleBorder}>
-        <div className={favoriteTitleStyle.favoriteTitleBlock}>
-          <div className={favoriteTitleStyle.test}>
-
-            {data.map((item) => (
-              <FavoriteCard
-                pictures={item.pictures}
-                index={item.index}
-                key={item._id}
-                id={item._id}
-                name={item.name}
-                price={item.price}
-                discount={item.discount}
-                stock={item.stock}
-              />
-            ))}
-
+    <div className={favoriteTitleStyle.favoriteTitleBlockBorderAll}>
+      {data.length === 0 && (
+        <div className={favoriteTitleStyle.test}>
+          <div className={favoriteTitleStyle.favoriteTitleBlockBorderZero}>
+            <div className={favoriteTitleStyle.favoriteTitleBlockAllZero}>
+              <div className={favoriteTitleStyle.favoriteTitleBlockContantZero}>
+                <hr />
+                <p>
+                  В избранном пусто, перейти в
+                  {' '}
+                  <Link to="/products">каталог</Link>
+                </p>
+                <hr />
+                <img src={logo} alt="logo" />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
+
+      {data.length > 0 && (
+        <div className={favoriteTitleStyle.favoriteTitleBlockAll}>
+          <div className={favoriteTitleStyle.favoriteTitleHeaderText}>
+            <h2>● Избранное ●</h2>
+          </div>
+          <div className={favoriteTitleStyle.favoriteTitleBorder}>
+            <div className={favoriteTitleStyle.favoriteTitleBlock}>
+              <div className={favoriteTitleStyle.test}>
+
+                {data.map((item) => (
+                  <FavoriteCard
+                    pictures={item.pictures}
+                    index={item.index}
+                    key={item._id}
+                    id={item._id}
+                    name={item.name}
+                    price={item.price}
+                    discount={item.discount}
+                    stock={item.stock}
+                  />
+                ))}
+
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,23 @@
 import mainStyle from './Main.module.css'
-import backgroundImageMain from '../Img/fonMainDog.jpg'
+import logo from '../Img/logoTwo.png'
+// import backgroundImageMain from '../Img/fonMainDog.jpg'
 
 export function Main() {
   return (
-    <div className={mainStyle.main}>
-      <img
-        className={mainStyle.mainBackgroundImgStyle}
-        src={backgroundImageMain}
-      />
+    <div className={mainStyle.backgorundMain}>
+      <div className={mainStyle.mainContainerBorder}>
+        <div className={mainStyle.mainContainer}>
+          <div className={mainStyle.mainContant}>
+            <div>
+              <h2 className={mainStyle.headerString}>
+                Добро пожаловать в HERITAGE
+              </h2>
+              <hr />
+              <img className={mainStyle.imgLogo} src={logo} alt="logo" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
